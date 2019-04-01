d94f017ed7eb009c08f456cf213ea6620059a82a
Fuse Jenkins <fuse-infra-team@redhat.com>
2019-04-01 19:13:54 +0000

    [1.5.b]: Update release config for 1.5.b

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,21 +1,21 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="version.fuse.online"
+git_fuse_online_install="1.5.b"
 
 # Tags used for the productised images
-tag_server="version.tag.server"
-tag_ui="version.tag.ui"
-tag_meta="version.tag.meta"
-tag_s2i="version.tag.s2i"
+tag_server="1.3-4"
+tag_ui="1.3-4"
+tag_meta="1.3-4"
+tag_s2i="1.3-4"
 
-tag_upgrade="version.tag.upgrade"
-tag_operator="version.tag.operator"
+tag_upgrade="1.3-5"
+tag_operator="1.3-5"
 
 
 # Docker repository for productised images
-repository="repo.name"
+repository="fuse7"
 
 # Test:
-registry="registry.name"
+registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
 
 # Official:
 # registry="registry.access.redhat.com"
